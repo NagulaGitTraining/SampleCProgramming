c30db22ecc73e629020c0b7b6c1e33fddbf67b21
Prasad <nagulaprasad19112018@gmail.com>
2018-12-12 16:11:46 -0500

    added some C program files

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+gcc -Wall main.c -o hello
+hello.exe
+@echo on
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+unsigned int loop;
+
+for (loop =0; loop<5; loop++)
+{
+printf("Hello world\n");	
+}
+return 0;
+}
